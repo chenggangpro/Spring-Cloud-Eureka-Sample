8593ec1ba820c414cc17d3c28877f7da5a185fa4
Cheng Gang <chenggangpro@gmail.com>
2020-03-09 17:13:29 +0800

    commit message

---
+++ eureka-client/src/test/java/pro/chenggang/project/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,11 @@
+package pro.chenggang.project.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
---
+++ eureka-server/src/test/java/pro/chenggang/project/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package pro.chenggang.project.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
